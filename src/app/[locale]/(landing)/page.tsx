b94dd3ae04204a2840d9b803e8b8bf87219e47e6
Adham Tarek <adhamtarek291@gmail.com>
2026-01-01 19:33:36 +0200

    remove unusd sections

--- src/app/[locale]/(landing)/page.tsx
+++ src/app/[locale]/(landing)/page.tsx
@@ -4,17 +4,16 @@ import type { Locale } from "next-intl";
 import { setRequestLocale } from "next-intl/server";
 import { use } from "react";
 
-import AboutSection from "./components/about-section";
-import ContactSection from "./components/contact-section";
-import CTASection from "./components/cta-section";
-import FAQSection from "./components/faq-section";
-import FeaturesSection from "./components/features-section";
-import GallerySection from "./components/gallery-section";
-import HeroSection from "./components/hero-section";
-import HowItWorksSection from "./components/how-it-works-section";
-import ServicesSection from "./components/services-section";
-import StatsSection from "./components/stats-section";
-import TestimonialsSection from "./components/testimonials-section";
+import About from "./components/about";
+import Contact from "./components/contact";
+import CTA from "./components/cta";
+import FAQ from "./components/faq";
+import Features from "./components/features";
+import Hero from "./components/hero";
+import HowItWorks from "./components/how-it-works";
+import Services from "./components/services";
+import Stats from "./components/stats";
+import Testimonials from "./components/testimonials";
 
 export default function IndexPage({ params }: PageProps) {
   const { locale } = use(params);
@@ -22,17 +21,16 @@ export default function IndexPage({ params }: PageProps) {
 
   return (
     <main>
-      <HeroSection />
-      <AboutSection />
-      <ServicesSection />
-      <HowItWorksSection />
-      <FeaturesSection />
-      <GallerySection />
-      <StatsSection />
-      <TestimonialsSection />
-      <FAQSection />
-      <ContactSection />
-      <CTASection />
+      <Hero />
+      <About />
+      <Services />
+      <HowItWorks />
+      <Features />
+      <Stats />
+      <Testimonials />
+      <FAQ />
+      <Contact />
+      <CTA />
     </main>
   );
 }
